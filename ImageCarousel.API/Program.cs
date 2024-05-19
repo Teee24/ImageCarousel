018111204e9d20a490e4f28a4846964ca316a70a
tina <tinalien0728@gmail.com>
2024-05-19 17:36:47 +0800

    feat : 新增輪播功能
    -設定Cors、statocoption

--- ImageCarousel.API/Program.cs
+++ ImageCarousel.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +26,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory()+ "\\wwwroot\\images")),
+    RequestPath = "/StaticFiles"
+
+    //設置/StaticFiles取代\wwwroot\\images路徑
+});
+Console.WriteLine();
 
 app.UseAuthorization();
 
